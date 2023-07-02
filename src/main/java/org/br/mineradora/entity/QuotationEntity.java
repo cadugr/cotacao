e671b8facbfc5e884adb49ce047de3f390d817ed
Cadu Guerra <cadu.guerra@gmail.com>
2023-07-02 19:23:22 -0300

    Criando o projeto do microserviço de cotação para o projeto de mineradora.

---
+++ src/main/java/org/br/mineradora/entity/QuotationEntity.java
@@ -0,0 +1,26 @@
+package org.br.mineradora.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "quotation")
+@Data
+@NoArgsConstructor
+public class QuotationEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Date date;
+    @Column(name = "currency_price")
+    private BigDecimal currencyPrice;
+    @Column(name = "pct_change")
+    private String pctChange; //representa o percentual de mudança do valor da moeda.
+    private String pair;
+
+}
